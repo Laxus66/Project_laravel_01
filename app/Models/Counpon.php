ab17fffbcaef41f84e51f720685eb38a92164509
Laxus66 <vinh11.dev@gmail.com>
2023-09-12 12:36:00 +0700

    Contruct Table

---
+++ app/Models/Counpon.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Counpon extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'name',
+        'type',
+        'value',
+        'expery_date'
+    ];
+}
